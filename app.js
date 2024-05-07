ab1044361af061ab61d1290c64cd561ccd43e02d
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2024-05-07 20:57:32 +0530

    Accounts & Apps in nav bar

--- app.js
+++ app.js
@@ -41,3 +41,39 @@ lucky_btn.onclick = function(){
     lucky_url = "https://doodles.google/";
     window.open(lucky_url);
 }
+
+//Adding Apps functionality for Apps Dropdown
+document.addEventListener('DOMContentLoaded', function() {
+    const appsButton = document.getElementById('apps-button');
+    const appsDropdown = document.getElementById('apps-dropdown');
+
+    appsButton.addEventListener('click', function() {
+        appsDropdown.style.display = (appsDropdown.style.display === 'block') ? 'none' : 'block';
+    });
+
+    // Close the dropdown menu if the user clicks outside of it
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('#apps-button')) {
+            appsDropdown.style.display = 'none';
+        }
+    });
+});
+
+
+
+// Adding JS for Account Icon
+document.addEventListener('DOMContentLoaded', function() {
+    const accountButton = document.getElementById('account-button');
+    const accountDropdown = document.getElementById('account-dropdown');
+
+    accountButton.addEventListener('click', function() {
+        accountDropdown.style.display = (accountDropdown.style.display === 'block') ? 'none' : 'block';
+    });
+
+    // Close the dropdown menu if the user clicks outside of it
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('#account-button')) {
+            accountDropdown.style.display = 'none';
+        }
+    });
+});
